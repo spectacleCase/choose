22e1b47730d08716f16099279f12db7f8ff1d275
桌角的眼镜 <13245209+lzt_luo@user.noreply.gitee.com>
2024-12-14 21:09:20 +0800

    修复地址显示

---
+++ choose-root/choose-model/src/main/java/com/choose/common/dto/GetAddressDitDto.java
@@ -0,0 +1,13 @@
+package com.choose.common.dto;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author 桌角的眼镜
+ * @version 1.0
+ * @since 2024/12/14 16:49
+ */
+public record GetAddressDitDto(String target,String address) {
+}
